5e7688ae66293bae97427fcbf7f03bee6b081b3d
Vesna Tan <tanvezi@gmail.com>
2018-10-06 14:52:51 -0400

    updated eslint configs

---
+++ src/components/SideDrawer/DrawerToggleButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DrawerToggleButton.css'
+
+const DrawerToggle = () => (
+  <button type="button">
+    <div className="toggle-buttom-line" />
+    <div className="toggle-buttom-line" />
+    <div className="toggle-buttom-line" />
+  </button>
+)
+
+export default DrawerToggle
